33f857ae106f901fb77eed4063614e59ed818ec8
Didinzz <didinzakaria123@gmail.com>
2023-03-01 00:13:16 +0800

    git commit "tambah create absen mahasiswa"

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once 'koneksi.php';
 
@@ -9,97 +9,97 @@ $data = mysqli_query($koneksi, 'SELECT * FROM mahasiswa');
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="#">SesukaHatiKalian</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="mahasiswa.php">Mahasiswa</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
-      <a class="navbar-brand" href="#">SesukaHatiKalian</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link" href="index.php">Home</a>
-          <a class="nav-link" href="mahasiswa.php">Mahasiswa</a>
+        <div class="row">
+            <div class="col">
+                <h1>Daftar Mahasiswa</h1>
+            </div>
         </div>
-      </div>
-    </div>
-  </nav>
 
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>Daftar Mahasiswa</h1>
-      </div>
-    </div>
+        <div class="row">
+            <div class="col">
+                <a href="create.php" class="btn btn-primary">Tambah data mahasiswa</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Nim</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $no = 1;
+                        while ($mahasiswa = mysqli_fetch_assoc($data)) : ?>
+                            <tr>
+                                <th scope="row"><?php echo $no ?></th>
+                                <td> <?php echo $mahasiswa['nama_mahasiswa'] ?></td>
+                                <td><?php echo $mahasiswa['NIM_mahasiswa'] ?></td>
+                                <td>
+                                    <button class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Detail</button>
+                                    <button class="btn btn-sm btn-warning">Edit</button>
+                                    <button class="btn btn-sm btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        <?php
+                            $no++;
+                        endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-    <div class="row">
-        <div class="col">
-            <a href="create.php" class="btn btn-primary">Tambah data mahasiswa</a>
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, aspernatur.
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-    <div class="col-md-8">
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Nim</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-    $no = 1;
-    while($mahasiswa = mysqli_fetch_assoc($data)) : ?>
-    <tr>
-      <th scope="row"><?php echo $no?></th>
-      <td> <?php echo $mahasiswa ['nama_mahasiswa'] ?></td>
-      <td><?php echo $mahasiswa ['NIM_mahasiswa'] ?></td>
-      <td>
-        <button class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Detail</button>
-        <button class="btn btn-sm btn-warning">Edit</button>
-        <button class="btn btn-sm btn-danger">Delete</button>
-      </td>
-    </tr>
-    <?php
-$no++; 
-    endwhile; ?>
-  </tbody>
-</table>
-    </div>
-  </div>
-
-  <!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, aspernatur.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
-  </div>
 
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
